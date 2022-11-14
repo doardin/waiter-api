f772c9570d01248e38217ae28f5abfc8bf4be157
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-11-14 20:21:12 -0300

    chore: Database connection config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
-
-app.listen(9000, () => {
-	console.log("🚀 Server is running on http://localhost:9000");
-});
+mongoose.connect("mongodb://localhost:27017")
+	.then(() => {
+		const app = express();
+		app.listen(9000, () => {
+			console.log("🚀 Server is running on http://localhost:9000");
+		});
+	})
+	.catch(() => console.log("Error when connect to database ❌"));
