f0e955125fe9e6b498e74270c91c7be3438df427
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-11-14 21:07:14 -0300

    feat: Use app routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import { router } from "./router";
 
 mongoose.connect("mongodb://localhost:27017")
 	.then(() => {
 		const app = express();
-		app.listen(9000, () => {
+		const port = 9000;
+
+		app.use(router);
+		app.listen(port, () => {
 			console.log("🚀 Server is running on http://localhost:9000");
 		});
 	})
