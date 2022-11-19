b4dcaa00511a13d68e0cad825a8a35f16dea0c16
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-11-19 13:20:38 -0300

    fix: Populate product when list orders

--- src/app/useCases/orders/listOrders.ts
+++ src/app/useCases/orders/listOrders.ts
@@ -3,6 +3,6 @@ import { Order } from "../../models/Order";
 
 
 export async function listOrders(req: Request, res: Response) {
-	const orders = await Order.find();
+	const orders = await Order.find().populate("products.product");
 	res.json(orders);
 }
