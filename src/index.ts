21da0850b11ee5c829ed21d6931900b65652bb3e
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-11-19 13:00:07 -0300

    chore: Express static to get images

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "node:path";
 import { router } from "./router";
 
 mongoose.connect("mongodb://localhost:27017")
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost:27017")
 		const app = express();
 		const port = 9000;
 
+		app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 		app.use(express.json());
 		app.use(router);
 
