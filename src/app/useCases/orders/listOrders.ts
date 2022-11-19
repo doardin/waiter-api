0c809a9c98db384ab95b36f19c9c8e72f360707a
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-11-19 13:22:30 -0300

    fix: Sort orders by createdAt

--- src/app/useCases/orders/listOrders.ts
+++ src/app/useCases/orders/listOrders.ts
@@ -3,6 +3,9 @@ import { Order } from "../../models/Order";
 
 
 export async function listOrders(req: Request, res: Response) {
-	const orders = await Order.find().populate("products.product");
+	const orders = await Order.find()
+		.sort({ createdAt:  -1 })
+		.populate("products.product");
+
 	res.json(orders);
 }
